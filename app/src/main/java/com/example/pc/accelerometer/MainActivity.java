c5e93f466711c72ccb469456e4551ce7107ebf66
dgonz1206 <dgonz1206@gmail.com>
2018-10-29 20:07:02 -0700

    degrees partially complete

--- app/src/main/java/com/example/pc/accelerometer/MainActivity.java
+++ app/src/main/java/com/example/pc/accelerometer/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.pc.accelerometer;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -43,11 +48,25 @@ public class MainActivity extends Activity implements SensorEventListener {
         SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
 
         // Assign TextView
-        xText = (TextView) findViewById(R.id.xText);
+        //xText = (TextView) findViewById(R.id.xText);
         yText = (TextView) findViewById(R.id.yText);
         zText = (TextView) findViewById(R.id.zText);
-    }
 
+        int repeatTime = 30;  //Repeat alarm time in seconds
+        AlarmManager processTimer = (AlarmManager)getSystemService(ALARM_SERVICE);
+        Intent intent = new Intent(this, processTimerReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0,  intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        //Repeat alarm every second
+        processTimer.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(),repeatTime*1000, pendingIntent);
+    }
+    //This is called every second (depends on repeatTime)
+    public class processTimerReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //Do something every 30 seconds
+        }
+    }
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         // Not in use
@@ -66,41 +85,59 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        xText.setText("X: " + event.values[0]);
-        yText.setText("Y: " + event.values[1]);
-        zText.setText("Z: " + event.values[2]);
-        File dir = new File(getFilesDir().toString());
-        Boolean dirsMade = dir.mkdir();
+        //xText.setText("X: " + sensorEvent.values[0]);
+        //yText.setText("Y: " + sensorEvent.values[1]);
+        //zText.setText("Z: " + sensorEvent.values[2]);
 
-        //Log.v("Accel", dirsMade.toString());
+        double x = event.values[0];
+        double y = event.values[1];
+        double z = event.values[2];
 
-        //File file = new File(dir, "output.csv");
+        double pitch = Math.atan(x/Math.sqrt(Math.pow(y,2) + Math.pow(z,2)));
+        double roll = Math.atan(y/Math.sqrt(Math.pow(x,2) + Math.pow(z,2)));
+        //convert radians into degrees
+        pitch = pitch * (180.0/3.14);
+        roll = roll * (180.0/3.14) ;
 
-        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "example.txt");
+        yText.setText("Axis: " + String.valueOf(pitch));
+        zText.setText("Axis: " + String.valueOf(roll));
 
-        String text = "X: " + event.values[0] + "\n" + "Y: " + event.values[1] +  "\n" + "Z: " + event.values[2];
-        FileOutputStream fos = null;
+//        xText.setText("X: " + event.values[0]);
+//        yText.setText("Y: " + event.values[1]);
+//        zText.setText("Z: " + event.values[2]);
+//        File dir = new File(getFilesDir().toString());
+//        Boolean dirsMade = dir.mkdir();
+//
+//        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "example.txt");
+//
+//        String text = "X: " + event.values[0] + "\n" + "Y: " + event.values[1] +  "\n" + "Z: " + event.values[2];
+//        FileOutputStream fos = null;
 
-        try {
-            fos = new FileOutputStream(file);
-            //fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
-            fos.write(text.getBytes());
-            fos.close();
-            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME,
-                    Toast.LENGTH_LONG).show();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (fos != null) {
-                try {
-                    fos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+//        try {
+//
+//            fos = new FileOutputStream(file);
+//            //fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+//            fos.write(text.getBytes());
+//            fos.close();
+//            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME,
+//                    Toast.LENGTH_LONG).show();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (fos != null) {
+//                try {
+//                    fos.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+
+
+
+
 //        try {
 //            FileOutputStream f = new FileOutputStream(file, true);
 //            try {
